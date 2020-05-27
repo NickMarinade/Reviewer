b9991185f9e1f1bd766fbf9a18abf0ebd2b43382
Mykola Marenich <marenich.mykola@gmail.com>
2020-05-28 01:06:43 +0200

    GET routes

--- api/controllers.js
+++ api/controllers.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const tv4 = require('tv4');
 const config = require('../config');
+const util = require("util");
 
 const SCHEMA = path.join(__dirname, '/..', config.DATA_DIR, '/reviewer-schema.json');
 const DATA_PATH = path.join(__dirname, '/..', config.DATA_DIR, '/reviewer-data.json');
@@ -17,6 +18,48 @@ const controllers = {
   hello: (req, res) => {
     res.json({ message: 'hello!' });
   },
+
+  getItems: async (req, res, next) => {
+    try {
+      const list = await readFilePromise(DATA_PATH, 'utf-8');
+      const parsedList = JSON.parse(list);
+      res.json(parsedList)
+    } catch (err) {
+      if (err && err.code === 'ENOENT') {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+      }
+    }
+  },
+
+  getItem: async (req, res, next) => {
+    const itemID = req.params.id;
+    const itemIDN = Number(itemID);
+
+    try {
+      const itemContent = await readFilePromise(DATA_PATH, 'utf-8');
+      const parsedContent = JSON.parse(itemContent);
+      const itemWithId = parsedContent.items.find(item => item.id === itemIDN);
+
+      if (itemWithId) {
+        res.json(itemWithId);
+      } else {
+        res.status(404).end("No such an item with the selected id");
+      }
+    } catch (err) {
+      if (err && err.code === 'ENOENT') {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+      }
+    }
+  },
+
 
 };
 
--- api/routes.js
+++ api/routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.get('/', controllers.hello);
 
 // write your routes
-
+router.get('/items', controllers.getItems);
+router.get('/items/:id', controllers.getItem);
 
 module.exports = router;
