efcc44568ffe44a8a1637844dd8f946a76ccf97c
Mykola Marenich <marenich.mykola@gmail.com>
2020-05-28 00:13:49 +0200

    npm+schema

--- api/controllers.js
+++ api/controllers.js
@@ -5,8 +5,13 @@ const path = require('path');
 const tv4 = require('tv4');
 const config = require('../config');
 
-const SCHEMA = path.join(__dirname, '/..', config.DATA_DIR, '/_-schema.json');
-const DATA_PATH = path.join(__dirname, '/..', config.DATA_DIR, '/_-data.json');
+const SCHEMA = path.join(__dirname, '/..', config.DATA_DIR, '/reviewer-schema.json');
+const DATA_PATH = path.join(__dirname, '/..', config.DATA_DIR, '/reviewer-data.json');
+
+const readFilePromise = util.promisify(fs.readFile);
+const writeFilePromise = util.promisify(fs.writeFile);
+const unlinkPromise = util.promisify(fs.unlink);
+const readdirPromise = util.promisify(fs.readdir);
 
 const controllers = {
   hello: (req, res) => {
