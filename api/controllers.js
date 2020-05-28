0224733f3aa679f1704aea491dd1c34d225a992f
Mykola Marenich <marenich.mykola@gmail.com>
2020-05-29 00:09:21 +0200

    POST+PUT+DELETE routes

--- api/controllers.js
+++ api/controllers.js
@@ -11,8 +11,6 @@ const DATA_PATH = path.join(__dirname, '/..', config.DATA_DIR, '/reviewer-data.j
 
 const readFilePromise = util.promisify(fs.readFile);
 const writeFilePromise = util.promisify(fs.writeFile);
-const unlinkPromise = util.promisify(fs.unlink);
-const readdirPromise = util.promisify(fs.readdir);
 
 const controllers = {
   hello: (req, res) => {
@@ -49,6 +47,127 @@ const controllers = {
       } else {
         res.status(404).end("No such an item with the selected id");
       }
+    } catch (err) {
+      if (err && err.code === 'ENOENT') {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+      }
+    }
+  },
+
+  createItem: async (req, res, next) => {
+    const newItem = req.body;
+
+    try {
+      const itemContent = await readFilePromise(DATA_PATH, 'utf-8');
+      const parsedContent = JSON.parse(itemContent);
+
+      newItem.id = parsedContent.nextId;
+      parsedContent.nextId++;
+
+      const isValid = tv4.validate(newItem, SCHEMA);
+
+      if (!isValid) {
+        const error = tv4.error;
+        console.error(error);
+
+        res.status(400).json({
+          error: {
+            message: error.message,
+            dataPath: error.dataPath
+          }
+        });
+        return
+      }
+
+      parsedContent.items.push(newItem);
+      
+      const dataToString = JSON.stringify(parsedContent, null, ' ');
+      await writeFilePromise(DATA_PATH, dataToString);
+      res.json(newItem);
+    } catch (err) {
+      if (err && err.code === 'ENOENT') {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+      }
+    }
+  },
+
+  updItem: async (req, res, next) => {
+    const updId = Number(req.params.id);
+    const item = req.body;
+    item.id = updId;
+    const isValid = tv4.validate(item, SCHEMA)
+
+
+    if (!isValid) {
+      const error = tv4.error
+      console.error(error)
+
+      res.status(400).json({
+        error: {
+          message: error.message,
+          dataPath: error.dataPath
+        }
+      })
+      return
+    }
+
+    try {
+      const itemContent = await readFilePromise(DATA_PATH, 'utf-8');
+      const parsedContent = JSON.parse(itemContent);
+
+      const entrytoUpdate = parsedContent.items.find(item => item.id === updId);
+
+      if (entrytoUpdate) {
+        const itemIndex = parsedContent.items.indexOf(entrytoUpdate);
+        parsedContent.items[itemIndex] = item;
+        const dataToString = JSON.stringify(parsedContent, null, ' ');
+        await writeFilePromise(DATA_PATH, dataToString);
+        res.json(item);
+      } else {
+        res.json('No item with requested id')
+      }
+    } catch (err) {
+      if (err && err.code === 'ENOENT') {
+        res.status(404).end();
+        return;
+      }
+      if (err) {
+        next(err);
+      }
+    }
+  },
+
+  deleteItem: async (req, res, next) => {
+    const itemId = req.params.id;
+    const idToDelete = Number(itemId);
+
+    try {
+      const itemContent = await readFilePromise(DATA_PATH, 'utf-8');
+      const parsedContent = JSON.parse(itemContent);
+
+      const entryToDelete = parsedContent.items
+        .find(item => item.id === idToDelete);
+      if (entryToDelete) {
+
+        parsedContent.items = parsedContent.items.filter(item => item.id !== entryToDelete.id);
+
+        const dataToString = JSON.stringify(parsedContent, null, '  ');
+
+        await writeFilePromise(DATA_PATH, dataToString);
+
+        res.json('Item deleted');
+      } else {
+        res.send('No item with requested id');
+      }
+
     } catch (err) {
       if (err && err.code === 'ENOENT') {
         res.status(404).end();
--- api/routes.js
+++ api/routes.js
@@ -8,5 +8,8 @@ router.get('/', controllers.hello);
 // write your routes
 router.get('/items', controllers.getItems);
 router.get('/items/:id', controllers.getItem);
+router.post('/items', controllers.createItem);
+router.put('/items/:id', controllers.updItem);
+router.delete('/items/:id', controllers.deleteItem);
 
 module.exports = router;
